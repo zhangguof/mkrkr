2fb24d61a09bd9e1c18d1e461368a922747c16a6
tony <zhangguof@gmail.com>
2018-06-03 21:38:35 +0800

    fps show.

--- src/Application.cpp
+++ src/Application.cpp
@@ -58,35 +58,64 @@ extern bool TVPProcessContinuousHandlerEventFlag;
 unsigned int Event_interval = 1000/60; // 1/60.0 *1000 ms
 
 static unsigned int last_tick = 0;
+static unsigned int fps_tick = 0;
+static tTJSNI_BaseLayer* gfps_lay = NULL;
+static ttstr fps_text("");
+void init_fps()
+{
+	if(gfps_lay) return;
+	if(!TVPMainWindow) return;
+	tTJSNI_BaseLayer* main_lay = TVPMainWindow->GetDrawDevice()->GetPrimaryLayer();
+	tTVPLayerManager* lay_m = main_lay->GetManager();
+	auto lays = lay_m->GetAllNodes();
+	
+	for(auto i:lays)
+	{
+		if(i->GetName() == TJS_W("fps"))
+		{
+			gfps_lay = i;
+			break;
+		}
+	}
+
+}
 static void render_update(unsigned int interval)
 {
 	// TVPAddLog(TJS_W("render_update!!!"));
 	unsigned int cur_tick = time_now();
-	if(cur_tick - last_tick > Event_interval)
+	
+	if(cur_tick - last_tick >= Event_interval)
 	{
 		TVPProcessContinuousHandlerEventFlag = true;
 		last_tick = cur_tick;
 	}
-	tTJSNI_BaseLayer* lay = TVPMainWindow->GetDrawDevice()->GetPrimaryLayer();
-	// ttstr text("");
-	// wprintf(TJS_W("%d,%.1f\n"),interval,1000.0/interval);
-	// text.printf(TJS_W("fps:%d"),1000/interval);
-	// TVPAddLog(text);
-	// wprintf(text.c_str());
-	// wprintf(TJS_W("\n"));
-	// lay->DrawText(lay->GetWidth()-100,0,
-	// text,0xffffff,
-	// 0xff,true,0,0,0,0,0);
+	if(gfps_lay)
+	{
+		
+		fps_text.printf(TJS_W("fps:%.1f"),1000.0/(cur_tick - fps_tick));
+		gfps_lay->FillRect(tTVPRect(0,0,
+				gfps_lay->GetWidth(),
+				gfps_lay->GetHeight()),
+		0x00000000);
+		gfps_lay->DrawText(0,0,fps_text,0x0000ff,
+		0xff,true,0,0,0,0,0);	
+	}
+
+
+
 
 
 	TVPDeliverAllEvents();
 	TVPMainWindow->UpdateContent();
+	// wprintf(TJS_W("=====:%d\n"),interval);
+	fps_tick = cur_tick;
 
 
 }
 void tTVPApplication::Run()
 {
 	// while(sdl_loop());
+	init_fps();
 	regist_update(render_update);
 	// if(TVPMainWindow)
 	// 	TVPMainWindow->UpdateContent();
