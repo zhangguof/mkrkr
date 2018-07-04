0b2bf74e8518a57244b5a10f3bf82943f02d4374
tony <zhangguof@gmail.com>
2018-07-04 20:13:32 +0800

    build in iOS and macOS.

--- src/core/main.c
+++ src/core/main.c
@@ -9,7 +9,11 @@
 //extern int init_core_test(int argc,char *argv[]);
 //in sdlinit.cpp
 //extern void sdl_loop();
+#ifdef __cplusplus
 extern "C" int app_main(int argc,char* argv[]);
+#else
+int app_main(int argc,char* argv[]);
+#endif
 
 int main(int argc, char *argv[])
 {
